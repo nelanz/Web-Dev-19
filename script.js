bdef386585a9aa939aa9fb535ab4253c4a65de2a
Nela Tomaszewicz <n.tomaszewicz@gmail.com>
2019-11-06 14:00:12 +0100

    Added for loop in JS and some simple functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var donationList = [5, 10, 25, 16, 40];
+
 function alertFunction() {
     alert('Uwaga formularz zostanie wyczyszczony!')
 }
@@ -14,7 +16,6 @@ function actionList() {
     document.getElementById("clicker").addEventListener(click, listen, false);
 }
 
-// var rand=((Math.random()* (100 - 40)) + 40);
 
 function listen() {
     document.getElementById("suggestion").innerHTML = "Pomożemy Ci wyznaczyć cel - spróbuj w tym roku przeznaczyć taką kwotę na" +
@@ -22,16 +23,60 @@ function listen() {
     return document.writeln((Math.random() * (100 - 40)) + 40).innerHTML;;
 }
 
-// function getRandomNumberOfPeople(min, max) {
-//     return Math.floor(Math.random * (max - min)) + min
-// }
 
-function question(){
+function question() {
     var checkBoxy = document.getElementById("knowY");
     var checkBoxn = document.getElementById("knowN");
-    var texty=document.getElementById("texty");
-    var textn=document.getElementById("textn");
-    if(checkBoxy.checked == true){ texty.style.display="block"; textn.style.display="none";}
-    else if (checkBoxn.checked==true){textn.style.display="block"; texty.style.display="none";}
-    else {textn.style.display="none"; texty.style.display="none";}
-  }
+    var texty = document.getElementById("texty");
+    var textn = document.getElementById("textn");
+    if (checkBoxy.checked == true) {
+        texty.style.display = "block";
+        textn.style.display = "none";
+    } else if (checkBoxn.checked == true) {
+        textn.style.display = "block";
+        texty.style.display = "none";
+    } else {
+        textn.style.display = "none";
+        texty.style.display = "none";
+    }
+}
+
+function printDonation() {
+    document.getElementById('donation').innerHTML = donation;
+}
+
+function isEmpty() {
+    if(document.forms["form"].donation.value === "") {
+        return false;
+    }
+    return true;
+}
+
+function addDonation() {
+    var donationVal = parseInt(document.getElementById("donation").value);
+    if (isEmpty() == false) {
+        alert("Niedźwiedzie polarne nie przezyją za 0 zł 😞")
+    } else {
+        donationList.push(donationVal);
+        document.getElementById("display-donation").innerHTML = "Dziękujemy za donację " + donationVal + " zł!";
+    }
+}
+
+function getDonators() {
+    var text = "";
+    var i;
+    for (i = 0; i < donationList.length; i++) {
+        text += "💚" + donationList[i];
+    }
+    document.getElementById("donations-list").innerHTML = text;
+}
+
+// function beAnonymous() {
+//     var checkAnonymous = document.getElementById("anonymous")
+//     if (checkAnonymous.checked == true) {
+//         document.getElementById("name").disabled = true;
+//     } else {
+//         document.getElementById("name").disabled = false;
+//     }
+// }
+
